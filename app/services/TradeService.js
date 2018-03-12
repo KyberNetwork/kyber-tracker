72edde55b26008a9013d33b7fb72265eccd7f354
DESKTOP-SQHTODC\ThinkKING <tuan.nguyentheanh@gmail.com>
2018-03-12 11:51:00 +0700

    Add api sum and count by marker address

--- app/services/TradeService.js
+++ app/services/TradeService.js
@@ -396,6 +396,42 @@ module.exports = BaseService.extends({
     });
   },
 
+  getCountMarkerAddress: function (markerAddress, fromDate, toDate, callback) {
+    const KyberTradeModel = this.getModel('KyberTradeModel');
+
+    let whereClauses = 'maker_address = ? AND block_timestamp > ? AND block_timestamp < ?';
+    let params = [markerAddress, fromDate, toDate];
+
+    KyberTradeModel.countGroupBy('maker_address', {
+      where: whereClauses,
+      params: params,
+      groupBy: ['maker_address']
+    }, (err, ret) => {
+      if (err) {
+        return callback(err);
+      }
+      return callback(null, ret);
+    });
+  },
+
+  getSumMarkerAddress: function (markerAddress, fromDate, toDate, callback) {
+    const KyberTradeModel = this.getModel('KyberTradeModel');
+
+    let whereClauses = 'maker_address = ? AND block_timestamp > ? AND block_timestamp < ?';
+    let params = [markerAddress, fromDate, toDate];
+
+    KyberTradeModel.sumGroupBy('volume_eth/1000000000000000000', {
+      where: whereClauses,
+      params: params,
+      groupBy: ['maker_address']
+    }, (err, ret) => {
+      if (err) {
+        return callback(err);
+      }
+      return callback(null, ret);
+    });
+  },
+
   _getGroupColumnByIntervalParam: function(interval) {
     switch (interval) {
       case 'H1':
