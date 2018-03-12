2c811fa6a3516de2d75e879b898b9360b528bece
An Nguyen <nguyenhuuan@gmail.com>
2018-03-12 09:30:19 +0700

    Handle db port.

--- config/Local.js
+++ config/Local.js
@@ -12,24 +12,28 @@ module.exports = {
       dbUser  : process.env.MYSQL_USERNAME,
       dbPwd   : process.env.MYSQL_PASSWORD,
       dbHost  : process.env.MYSQL_DB_HOST,
+      dbPort  : process.env.MYSQL_DB_PORT,
     },
     'mysql-slave' : {
       dbName  : process.env.MYSQL_DBNAME,
       dbUser  : process.env.MYSQL_USERNAME,
       dbPwd   : process.env.MYSQL_PASSWORD,
       dbHost  : process.env.MYSQL_DB_HOST,
+      dbPort  : process.env.MYSQL_DB_PORT,
     },
     'mysql-master-test' : {
       dbName  : process.env.MYSQL_DBNAME,
       dbUser  : process.env.MYSQL_USERNAME,
       dbPwd   : process.env.MYSQL_PASSWORD,
       dbHost  : process.env.MYSQL_DB_HOST,
+      dbPort  : process.env.MYSQL_DB_PORT,
     },
     'mysql-slave-test' : {
       dbName  : process.env.MYSQL_DBNAME,
       dbUser  : process.env.MYSQL_USERNAME,
       dbPwd   : process.env.MYSQL_PASSWORD,
       dbHost  : process.env.MYSQL_DB_HOST,
+      dbPort  : process.env.MYSQL_DB_PORT,
     },
   }
 }
