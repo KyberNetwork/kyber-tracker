7b59c790cf10337092af95abe8acd59aea00bfc7
thith <thi1981@gmail.com>
2018-07-03 00:28:56 +0700

    Add API

--- app/services/CurrenciesService.js
+++ app/services/CurrenciesService.js
@@ -143,20 +143,20 @@ module.exports = BaseService.extends({
     async.auto({
       baseVolume: (next) => {
         KyberTradeModel.sum('volume_eth', {
-          where: 'block_timestamp > ? AND ((maker_token_symbol = ? AND taker_token_symbol = ?) OR (maker_token_symbol = ? AND taker_token_symbol = ?))',
-          params: [nowInSeconds - DAY_IN_SECONDS, tokenSymbol, base, base, tokenSymbol],
+          where: 'block_timestamp > ? AND (maker_token_symbol = ? OR taker_token_symbol = ?)',
+          params: [nowInSeconds - DAY_IN_SECONDS, tokenSymbol, tokenSymbol],
         }, next);
       },
       quoteVolumeTaker: (next) => {
         KyberTradeModel.sum('taker_token_amount', {
-          where: 'block_timestamp > ? AND maker_token_symbol = ? AND taker_token_symbol = ?',
-          params: [nowInSeconds - DAY_IN_SECONDS, base, tokenSymbol],
+          where: 'block_timestamp > ? AND taker_token_symbol = ?',
+          params: [nowInSeconds - DAY_IN_SECONDS, tokenSymbol],
         }, next);
       },
       quoteVolumeMaker: (next) => {
         KyberTradeModel.sum('maker_token_amount', {
-          where: 'block_timestamp > ? AND maker_token_symbol = ? AND taker_token_symbol = ?',
-          params: [nowInSeconds - DAY_IN_SECONDS, tokenSymbol, base],
+          where: 'block_timestamp > ? AND maker_token_symbol = ?',
+          params: [nowInSeconds - DAY_IN_SECONDS, tokenSymbol],
         }, next);
       },
       price: (next) => {
@@ -164,8 +164,8 @@ module.exports = BaseService.extends({
       },
       lastTrade: (next) => {
         KyberTradeModel.findOne({
-          where: '(taker_token_symbol = ? and maker_token_symbol = ?) or (taker_token_symbol = ? and maker_token_symbol = ?)',
-          params: [tokenSymbol, base, base, tokenSymbol],
+          where: 'taker_token_symbol = ? OR maker_token_symbol = ?',
+          params: [tokenSymbol, tokenSymbol],
           orderBy: 'block_timestamp DESC',
         }, next)
       }
@@ -179,15 +179,15 @@ module.exports = BaseService.extends({
         let bigMakerAmount = ret.lastTrade.makerTokenAmount ? new BigNumber(ret.lastTrade.makerTokenAmount) : new BigNumber(0)
         let bigTakerAmount = ret.lastTrade.takerTokenAmount ? new BigNumber(ret.lastTrade.takerTokenAmount) : new BigNumber(0)
 
-        if(ret.lastTrade.takerTokenSymbol == base && !bigMakerAmount.isZero()){
-          let amountTaker = bigTakerAmount.div(Math.pow(10, baseTokenData.decimal))
-          let amountMaker = bigMakerAmount.div(Math.pow(10, tokenData.decimal))
-          lastPrice = amountTaker.div(amountMaker).toNumber()
+        if(ret.lastTrade.takerTokenSymbol != tokenSymbol && !bigMakerAmount.isZero()){
+          let amountTaker = ret.lastTrade.volumeEth; //bigTakerAmount.div(Math.pow(10, baseTokenData.decimal))
+          let amountMaker = bigMakerAmount.div(Math.pow(10, tokenData.decimal));
+          lastPrice = new BigNumber(amountTaker).div(amountMaker).toNumber()
         }
-        else if(ret.lastTrade.makerTokenSymbol == base && !bigTakerAmount.isZero()){
-          let amountMaker = bigMakerAmount.div(Math.pow(10, baseTokenData.decimal))
+        else if(ret.lastTrade.makerTokenSymbol != tokenSymbol && !bigTakerAmount.isZero()){
+          let amountMaker = ret.lastTrade.volumeEth; //bigMakerAmount.div(Math.pow(10, baseTokenData.decimal))
           let amountTaker = bigTakerAmount.div(Math.pow(10, tokenData.decimal))          
-          lastPrice = amountMaker.div(amountTaker).toNumber()
+          lastPrice = new BigNumber(amountMaker).div(amountTaker).toNumber()
         }
 
       }
@@ -208,6 +208,117 @@ module.exports = BaseService.extends({
         lastTimestamp: ret.lastTrade ? ret.lastTrade.blockTimestamp : null,
         baseVolume: ret.baseVolume,
         quoteVolume: quoteVolume
+      })
+    })
+  },
+
+  getPair24hData: function (callback) {
+    if(!tokens) return callback(null, {});
+
+    let pairs = {};
+
+    Object.keys(tokens).map(token => {
+      if((token.toUpperCase() !== "ETH") && 
+          !tokens[token].delisted &&
+          helper.shouldShowToken(token)){
+        pairs["ETH_" + token] = (asyncCallback) => this._getPair24hData({
+          tradeAdapter: this.getModel('KyberTradeModel').getSlaveAdapter(),
+          rateAdapter: this.getModel('RateModel').getSlaveAdapter(),
+          token: token,
+          fromCurrencyCode: "ETH"
+        }, asyncCallback)
+      }
+    });
+
+    pairs.allRates = this.getService('CMCService').getAllRates;
+
+    async.auto(pairs, function(err, pairs){
+      if (!err) {
+        const rates = pairs.allRates;
+        delete pairs.allRates;
+        Object.values(pairs).forEach((value) => {
+          const askrate = rates.getRate(value.base_symbol, value.quote_symbol);
+          const normalAsk = askrate ? (1 / askrate) : 0;
+          const normalBid = rates.getRate(value.quote_symbol, value.base_symbol);
+          if (normalAsk) {
+            value.current_ask = normalAsk;
+          }
+          if (value.current_ask > value.past_24h_high) {
+            value.past_24h_high = value.current_ask;
+          }
+
+          if (normalBid) {
+            value.current_bid = normalBid;
+          }
+          if (value.current_bid < value.past_24h_low) {
+            value.past_24h_low = value.current_bid;
+          }
+
+        });
+      }
+
+      callback(err, pairs);
+    });
+  },
+
+  _getPair24hData: function (options, callback) {
+    if(!options.token || !tokens[options.token]){
+      return callback("token not supported")
+    }
+
+    if(!options.fromCurrencyCode || !tokens[options.fromCurrencyCode]){
+      return callback("base not supported")
+    }
+
+    let tokenSymbol = options.token
+    let baseSymbol = options.fromCurrencyCode
+
+    const nowInMs = Date.now();
+    const nowInSeconds = Math.floor(nowInMs / 1000);
+    const DAY_IN_SECONDS = 24 * 60 * 60;
+    const dayAgo = nowInSeconds - DAY_IN_SECONDS;
+
+    // volume SQL
+    const tradeWhere = 'block_timestamp > ? AND (maker_token_symbol = ?  OR  taker_token_symbol = ?)';
+    const tradeSql = `select IFNULL(sum(volume_usd),0) as usd_24h_volume from kyber_trade where ${tradeWhere}`;
+    const tradeParams = [dayAgo, tokenSymbol, tokenSymbol];
+
+    // 24h price SQL
+    const rateSql = `SELECT max(buy_expected) as 'past_24h_high', min(sell_expected) as 'past_24h_low' FROM rate
+      WHERE quote_symbol = ? AND block_timestamp > ? AND sell_expected > 0`;
+    const rateParams = [tokenSymbol, dayAgo];
+
+    // last price SQL
+    const lastSql = `SELECT buy_expected as 'current_ask', sell_expected as 'current_bid' FROM rate
+      WHERE quote_symbol = ? AND buy_expected > 0 AND sell_expected > 0
+      ORDER BY block_number DESC LIMIT 1`;
+    const lastParams = [tokenSymbol];
+
+    async.auto({
+      trade: (next) => {
+        options.tradeAdapter.execRaw(tradeSql, tradeParams, next);
+      },
+      rate: (next) => {
+        options.rateAdapter.execRaw(rateSql, rateParams, next);
+      },
+      latest: (next) => {
+        options.rateAdapter.execRaw(lastSql, lastParams, next);
+      }
+    }, (err, ret) => {
+      console.log(ret);
+      if (err) {
+        return callback(err);
+      }
+
+      return callback( null , {
+        timestamp: nowInMs,
+        quote_symbol: tokenSymbol,
+        base_symbol: baseSymbol,
+        past_24h_high: ret.rate[0].past_24h_high || 0,
+        past_24h_low: ret.rate[0].past_24h_low || 0,
+        usd_24h_volume: ret.trade[0].usd_24h_volume || 0,
+        current_bid: ret.latest[0].current_bid || 0,
+        current_ask: ret.latest[0].current_ask || 0
       })
     })
   },
