af43dffd55f0da4b34a21b57a498efa41d8f77fa
smith pham <phamngocvances@gmail.com>
2018-08-09 14:05:25 +0700

    add list config request rate limited

--- app/middlewares/RateLimit.js
+++ app/middlewares/RateLimit.js
@@ -1,9 +1,17 @@
 const RateLimit = require('express-rate-limit');
-
+const configList = require('../../config/ratelimit');
 module.exports = () => {
-    return new RateLimit({
-      windowMs: 15*60*1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-      delayMs: 0 // disable delaying - full speed until the max limit is reached
-    });
+  return new RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    delayMs: 0, // disable delaying - full speed until the max limit is reached
+    skip: function (req, res) {
+      for (let index in configList) {
+        if (req.url.indexOf(configList[index]) === 1) {
+          return false
+        }
+      }
+      return true
+    }
+  });
 };
