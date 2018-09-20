f59641f3e8ca6b76bc5c7e6b7cdb9c6d64551dc2
Tuan Nguyen <edwardevans094@gmail.com>
2018-09-20 11:53:33 +0700

    remove weth icon

--- config/network/staging.js
+++ config/network/staging.js
@@ -534,7 +534,6 @@ module.exports = {
         "symbol" : "WETH",
         "decimal": 18,
         "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-        "icon": "weth.png",
         "hidden": 1536846367000
       }
     },
