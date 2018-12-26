d99abe9246bcb69ccef32fb98b30d40f36221be5
Tuan Nguyen <edwardevans094@gmail.com>
2018-12-26 15:49:20 +0700

    update sql

--- db/alter_schema/update_tradelog_multiple_trade.sql
+++ db/alter_schema/update_tradelog_multiple_trade.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `kyber_tracker`.`kyber_trade` 
+DROP INDEX `tx_UNIQUE` ;
+
+ALTER TABLE `kyber_tracker`.`kyber_trade` 
 ADD COLUMN `unique_tag` VARCHAR(100) NOT NULL AFTER `updated_by`,
 ADD UNIQUE INDEX `unique_tag_UNIQUE` (`unique_tag` ASC);
