261581d655c5f4c90930b5614e12824e389025c5
Tuan Nguyen <edwardevans094@gmail.com>
2019-01-21 11:42:26 +0700

    fix export csv

--- resources/assets/js/home/components/SearchResultPage.vue
+++ resources/assets/js/home/components/SearchResultPage.vue
@@ -184,8 +184,11 @@ export default {
           let dataCSV = encodeURI(csvData);
 
           let link = document.createElement('a');
-          link.setAttribute('href', dataCSV);
-          link.setAttribute('download', new Date().toUTCString() + " " + q);
+          link.href = dataCSV
+          link.target = '_blank'
+          link.download = new Date().toUTCString() + " " + q + '.csv'
+          
+          document.body.appendChild(link);
           link.click();
         });
     }
