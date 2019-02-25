9488423773e09bda6a0024531fe8aecbe24784ea
Tuan Nguyen <edwardevans094@gmail.com>
2019-02-25 15:20:00 +0700

    fix return json pairs

--- app/controllers/CurrenciesController.js
+++ app/controllers/CurrenciesController.js
@@ -64,12 +64,10 @@ module.exports = AppController.extends({
     redisCacheService.getCacheByKey(CACHE_KEY, (err, ret) => {
       if (err) {
         logger.error(err)
-        res.json(ret);
-        return;
+        return res.send(err);
       }
       if (ret) {
-        res.send(ret);
-        return;
+        return res.json(JSON.parse(ret));
       }
       CurrenciesService.getConvertiblePairs((err, rett) => {
         if (err) {
