30b39d22ac1be4fb762341a8129420f26ba49128
Tuan Nguyen <edwardevans094@gmail.com>
2019-07-15 09:47:55 +0700

    staging equard

--- config/network/staging.js
+++ config/network/staging.js
@@ -861,6 +861,13 @@ module.exports = {
         "symbol" : "MYB",
         "decimal": 18,
         "address": "0x5d60d8d7ef6d37e16ebabc324de3be57f135e0bc"
+      },
+
+      "EQUAD": {
+        "name": "Quadrant Protocol",
+        "symbol" : "EQUAD",
+        "decimal": 18,
+        "address": "0xC28e931814725BbEB9e670676FaBBCb694Fe7DF2"
       }
     },
     "averageBlockTime": 15000,
