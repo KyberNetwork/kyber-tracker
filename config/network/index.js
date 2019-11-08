edcf94e4a7d965dc8ac1441f7d9e5fc02afa33d2
Tuan Nguyen <edwardevans094@gmail.com>
2019-11-08 11:40:03 +0700

    change get config

--- config/network/index.js
+++ config/network/index.js
@@ -4,8 +4,19 @@ const ropstenSettings = require('./ropsten');
 const kovanSettings = require('./kovan');
 
 module.exports = (function(chain) {
-    if (chain === "ropsten") return ropstenSettings;
-    if (chain === "staging") return stagingSettings;
-    if (chain === "kovan") return kovanSettings;
-    return mainnetSettings;
+    try {
+        const configData = require('./' + chain );
+        return configData
+    } catch (err) {
+        if (err.code && err.code === 'MODULE_NOT_FOUND') {
+            console.error('No config file matching ENV=' + chain);
+            // process.exit(1);
+          } else {
+            throw err;
+          }
+    }
+    // if (chain === "ropsten") return configPath;
+    // if (chain === "staging") return stagingSettings;
+    // if (chain === "kovan") return kovanSettings;
+    // return mainnetSettings;
 })(process.env.NODE_ENV || "production");
